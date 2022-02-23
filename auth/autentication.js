c6d14e99c4bf74118f9603b069e396b8788bfb07
Leonardorossato <leonardorossato21@gmail.com>
2022-02-23 14:47:10 -0300

    arquivos com as verificações para senha e para email

---
+++ auth/autentication.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
+const Users = require('../models/Users')
+const { InvalidArgumentError} = require('../middleware/erros')
+
+const verifyUser = (user) => {
+    if(!user) {
+        throw new InvalidArgumentError('Não existe usario com esse email')
+    }
+}
+
+const verifyPassword = async (senha, senhaHash) => {
+    const senhaValida = await bcrypt.compare(senha, senhaHash)
+    if(!senhaValida) {
+        throw new InvalidArgumentError('Email ou senha invalidos')
+    }
+}
+
+passport.use(
+    new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'senha', 
+    session: false,
+},async(email, senha, done)=>{
+        try {
+            const user = await Users.buscaPorEmail(email)
+            verifyUser(user)
+            await verifyPassword(senha, user.senhaHash)
+            done(null, user);
+        } catch (error) {
+            done(error)
+        }
+    })
+)
