e2d942449c233ae03e2603b82b031f1fec6d7741
Leonardorossato <leonardorossato21@gmail.com>
2022-02-23 15:08:51 -0300

    lib passaport usada para a rota login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 const usersControllers = require('../controllers/usersControllers');
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
 router.post('/add', usersControllers.adiciona)
 router.get('/', usersControllers.lista);
-
-router.delete('/usuario/:id', usersControllers.deleta);
+router.delete('/deleta/:id', usersControllers.deleta);
+router.post('/login', passport.authenticate('local', {session: false}), usersControllers.login)
 
 module.exports = router;
   
