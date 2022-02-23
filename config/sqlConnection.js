9973433b81c796d9ceba6969a48f7a7afa89d931
Leonardorossato <leonardorossato21@gmail.com>
2022-02-23 10:13:21 -0300

    n utilizado mais

--- config/sqlConnection.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('blog-db', 'users', 'pass',{
-    dialect: 'sqlite',
-    host: 'blog-codigo.sqlite',
-});
-
-module.exports = sequelize
