5759976545b51f8ec38cebbb6324ad310ac9bf63
Leonardorossato <leonardorossato21@gmail.com>
2022-02-23 10:13:10 -0300

    ajuste no import

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
-const schema = require('./database/schema');
+const bodyParser = require('body-parser')
+const db = require('./database/schema');
+const postRouter = require('./routes/postRouter')
+const userRouter = require('./routes/userRouter')
 require('dotenv').config()
 const port = process.env.PORT 
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-schema.sync().then(() => {
-    console.log('Connection successful with Mysql Workbench')
-}).catch(err => {
-    console.log('Connection failed with Mysql error: ' + err)
-})
+app.use('/api/users', userRouter)
+app.use('/api/posts', postRouter)
+
+app.db = db
 
 
 
