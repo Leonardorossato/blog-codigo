471855bf65275f323f359230f76c2d5d2727c6c8
Leonardorossato <leonardorossato21@gmail.com>
2022-02-24 09:34:17 -0300

    middleware customizado do token

--- middleware/autentication.js
+++ middleware/autentication.js
@@ -6,14 +6,25 @@ module.exports = {
             if (error && error.name === 'InvalidArgumentError') {
                 return res.status(401).json({error: error.message});
             }else if (erro){
-                return res.status(500).json({error: erro.message});
+                return res.status(500).json({error: error.message});
             }else if (!user) {
-                return res.status(401).json({error: erro.message});
+                return res.status(401).json({error: error.message});
             }
-
             req.user = user;
             return next()
         },(req, res, next))
+    },
+    bearer: (req, res, next) => {
+        passport.authenticate('bearer', {session: false}, (error, user, info)=>{
+            if (error && error.name === 'JsonwebtokenError') {
+                return res.status(401).json({error: error.message})
+            }else if (error){
+                return res.status(500).json({error: error.message})
+            }else if (!user){
+                return res.status(401).json()
+            }
+            req.user = user;
+            return next();
+        })
     }
-    
 }
