28911c6b3c792b6ea46fdfe75305206118037ae0
Leonardorossato <leonardorossato21@gmail.com>
2022-02-24 09:20:41 -0300

    substituindo o passport pelo custom

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const usersControllers = require('../controllers/usersControllers');
+const autentication = require('../middleware/autentication')
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
 router.post('/add', usersControllers.adiciona)
 router.get('/', usersControllers.lista);
-router.delete('/deleta/:id', passport.authenticate('bearer', {session: false}), usersControllers.deleta);
+router.delete('/deleta/:id', autentication.local, usersControllers.deleta);
 router.post('/login', passport.authenticate('local', {session: false}), usersControllers.login)
 
 module.exports = router;
