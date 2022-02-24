0487bcdea81d303a3d89dec442eef5e6f6a6e9de
Leonardorossato <leonardorossato21@gmail.com>
2022-02-24 09:42:45 -0300

    ok

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,12 +2,11 @@ const usersControllers = require('../controllers/usersControllers');
 const autentication = require('../middleware/autentication')
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 
 router.post('/add', usersControllers.adiciona)
 router.get('/', usersControllers.lista);
 router.delete('/deleta/:id', autentication.local, usersControllers.deleta);
-router.post('/login', passport.authenticate('local', {session: false}), usersControllers.login)
+router.post('/login', autentication.bearer, usersControllers.login)
 
 module.exports = router;
   
